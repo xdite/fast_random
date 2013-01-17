6ccd2cdedbe4224ab8e71d2147a7a5f1b32d2b20
Richard Lee <dlackty@gmail.com>
2013-01-17 17:50:05 +0800

    Fix deprecation warning of InstanceMethods.

--- lib/fast_random.rb
+++ lib/fast_random.rb
@@ -2,14 +2,11 @@
 require 'active_support/concern'
 module FastRandom
   extend ActiveSupport::Concern
-  
+
   module ClassMethods
     def random
       joins("join (SELECT CEIL(RAND() * (SELECT MAX(id) FROM #{table_name})) AS gid) AS r2").where("#{table_name}.id > r2.gid")
     end
   end
-  
-  module InstanceMethods
-  end
-  
+
 end
